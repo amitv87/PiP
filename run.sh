116186d1c32435b586edd83e98b3c79f7c107ee7
amit verma <amitv87@gmail.com>
2017-12-11 00:11:47 +0530

    console app to cocoa

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-clang pip/*.m -o bin -fobjc-arc -fobjc-link-runtime -framework Cocoa -framework CoreGraphics -framework QuartzCore -framework OpenGL -O3 && ./bin
+mkdir -p build
+APP=build/PiP
+clang pip/*.m -o $APP -fobjc-arc -fobjc-link-runtime -framework Cocoa -framework CoreGraphics -framework QuartzCore -framework OpenGL -O3 && ./$APP
