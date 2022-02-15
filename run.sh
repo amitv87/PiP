051694420125f17116739d91dc261fc1e17ac006
boggyb <amitv87@gmail.com>
2022-02-15 23:28:15 +0530

    build fix

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 mkdir -p build
 APP=build/PiP
-clang pip/*.m -o $APP -fobjc-arc -fobjc-link-runtime \
-  -framework Cocoa -framework QuartzCore -framework OpenGL -framework Metal -framework MetalKit \
-  /System/Library/PrivateFrameworks/PIP.framework/PIP /System/Library/PrivateFrameworks/SkyLight.framework/SkyLight -O3 -g0 -Wno-deprecated-declarations && ./$APP
+clang pip/*.m -o $APP -fobjc-arc -fobjc-link-runtime -O3 -g0 -Wno-deprecated-declarations -F /System/Library/PrivateFrameworks \
+  -framework Cocoa -framework QuartzCore -framework OpenGL -framework Metal -framework MetalKit -framework PIP -framework SkyLight && ./$APP
