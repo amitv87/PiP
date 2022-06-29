d3013f149fad2fdf8c72253c1995033741450c8f
boggyb <amitv87@gmail.com>
2022-06-29 14:43:31 +0530

    better packager

--- package.sh
+++ package.sh
@@ -1,2 +1,14 @@
-VERSION=$1
-dmgbuild -s settings.py PiP PiP-$VERSION.dmg
+set -ex
+
+SRC_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SRC_ROOT_DIR
+
+xcodebuild -alltargets archive
+
+APP_DIR="build/Release/PiP.app"
+PLIST="$APP_DIR/Contents/Info.plist"
+
+NAME=$(/usr/libexec/PlistBuddy -c "Print :CFBundleName" $PLIST)
+VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" $PLIST)
+
+dmgbuild -s settings.py -D app=$APP_DIR $NAME build/$NAME-$VERSION.dmg
